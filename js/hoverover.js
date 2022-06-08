588348636a5bb7e45f4b2880bb16ecae4c9e6983
ALCATRAAAZ <jekabsons.alberts@gmail.com>
2022-06-08 17:26:45 +0300

    JS page functionality updates

--- js/hoverover.js
+++ js/hoverover.js
@@ -1,24 +1,44 @@
-//creating a button to be trigered when enterinh .image-grid
+//creating a function button to be trigered when entering section div div
 
-const button = document.querySelector('.image-grid');
+function buttonHover() {
+  const button3 = document.querySelector('section div div');
+  //when cursor enters section div div event is trigered
+  button3.addEventListener(
+    'mouseover',
+    (event) => {
+      //create an empty border for all img when entering the section
+      document
+        .querySelectorAll('img')
+        .forEach((a) => a.setAttribute('border', ''));
+      //adding color to an image which is targeted
+      event.target.style.borderColor = 'rgb(246,225,197)';
+      //adding border style to an image which is targeted
+      event.target.setAttribute('border', '2rem');
+    },
+    false
+  );
+}
+buttonHover();
 
-//when cursor enters image-grid evevt 2 is trigered
-button.addEventListener('mouseover', (event) => {
-  //debuging the event
-  //console.log(event);
-  //event.preventDefault();
-  //image = document.querySelector('img'); - is the event target
+function buttonClick() {
+  //create an empty arrar to store elements for img when clicking
+  let clicks = {};
+  function reportClicks(item) {
+    clicks[item] = clicks[item] + 1 || 1;
+    console.log(item, clicks);
+  }
+  return reportClicks;
+}
+reportActivities = buttonClick();
 
-  event.target.setAttribute('border', '1px');
-  //once the target is hovered over the color of image border changes to 1px 'rgb(246,225,197)'
-  event.target.style.borderColor = 'rgb(246,225,197)';
-});
-
-button.addEventListener('mouseleave', (event2) => {
-  //debuging the event
-  //console.log(event2);
-
-  //once the target is not hovered over the color of all images border changes to ''
-  a = document.querySelectorAll('img');
-  document.querySelectorAll('img').forEach((a) => a.setAttribute('border', ''));
-});
+document.querySelector('.image-grid').addEventListener(
+  'click',
+  (event2) => {
+    reportActivities(event2.target.title);
+    event2.target.setAttribute('border', '6rem');
+    // Storage = event2.target.title;
+    // console.log(Storage);
+  },
+  false
+)();
+buttonClick();
