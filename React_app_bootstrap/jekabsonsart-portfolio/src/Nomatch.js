7e07acb3253f693e5d87c879ef8d005fb9f38c15
Alberts Jekabsons <98735207+ALCATRAAAZ@users.noreply.github.com>
2025-02-28 14:57:30 +0200

    Delete React_app_bootstrap/jekabsonsart-portfolio directory

--- React_app_bootstrap/jekabsonsart-portfolio/src/Nomatch.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Nomatch() {
-  return (
-    <div>
-      <h1>404 ERROR</h1>
-     </div>
-  );
-}
--- React_app_bootstrap/jekabsonsart-portfolio/src/components/Layout.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Container from 'react-bootstrap/container';
-
-export default function Layout(props) {
-  return <Container>{props.children}</Container>;
-}
